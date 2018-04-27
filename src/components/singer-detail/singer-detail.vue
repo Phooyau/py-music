0d62aa58aa0babec0df0416b6128ea9c86d53f23
傅瑶 <fuyao@bwton.com>
2018-04-27 18:55:40 +0800

    播放器内置组件开发1

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -68,6 +68,7 @@
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
-  .slide-enter, .slider-leave-to
-   transform: translate3d(100%, 0, 0)
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
 </style>
